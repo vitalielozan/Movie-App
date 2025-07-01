c00e47cf026f2f06995931a62c6293b49613aa83
VitalieLozan <vitalielozan17@outlook.com>
2025-07-01 12:55:44 +0200

    new commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,7 @@ function Footer() {
   return (
     <footer className="footer footer-center bg-gray-100 p-8 text-base-content dark:bg-base-300">
       <aside>
-        <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by
-          Vitalie Lozan
-        </p>
+        <p>Copyright © {new Date().getFullYear()} - Vitalie Lozan.</p>
       </aside>
     </footer>
   );
